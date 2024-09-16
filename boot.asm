8b50c63e6dddac923ab18beda64816c138fc7968
orangecalculator <smartman1996@gmail.com>
2024-09-16 09:15:35 +0000

    Print some register content after initialization.

--- boot.asm
+++ boot.asm
@@ -2,8 +2,18 @@ ORG 0x7C00
 BITS 16
 
 start:
+  mov cx, ss
+  mov dx, sp
   mov si, hellomessage
   call printstr
+  mov ax, cx
+  call printhex
+  mov ax, dx
+  call printhex
+  mov ax, cs
+  call printhex
+  lea ax, $
+  call printhex
   jmp $
 
 ; TODO: Where is the stack pointing to when initialized?
@@ -18,6 +28,38 @@ printstr:
 .finish_printchar:
   ret
 
+printhex:
+  mov bx, 0
+  mov si, ax
+  shr ax, 12
+  and ax, 0xf
+  call printhexchar
+  mov ax, si
+  shr ax, 8
+  and ax, 0xf
+  call printhexchar
+  mov ax, si
+  shr ax, 4
+  and ax, 0xf
+  call printhexchar
+  mov ax, si
+  and ax, 0xf
+  call printhexchar
+  ret
+
+printhexchar:
+  cmp al, 10
+  jge .alpha
+.numeric:
+  add al, '0'
+  jmp .print
+.alpha:
+  add al, 'A' - 10
+.print:
+  call printchar
+  ret
+
+; Arguments: character in al, cursor in bx
 printchar:
   mov ah, 0x0e
   int 0x10
