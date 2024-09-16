8c780d195c207ce396a30cd383f26cf8aaa12eab
orangecalculator <smartman1996@gmail.com>
2024-09-16 09:32:55 +0000

    Add more description about processor state after BIOS initialization.

--- boot.asm
+++ boot.asm
@@ -3,10 +3,21 @@ BITS 16
   ; Actually, most of the instructions are position independent,
   ; so the origin settings will not likely matter.
 
-  ; At this point, this binary is loaded into 0X7C00
+  ; At this point, this binary is loaded into 0X7C00 and executed in real mode.
   ; See also: https://www.glamenv-septzen.net/en/view/6
   ; See also: IBM Personal Computer Hardware Reference Library, Technical Reference
   ; See also: Ralf Brown's interrupt list, INT 19 - SYSTEM - BOOTSTRAP LOADER
+  ;
+  ; Note that the stack is not guaranteed to point to a suitable location.
+  ; See also: https://retrocomputing.stackexchange.com/questions/2927/did-the-intel-8086-8088-not-guarantee-the-value-of-sssp-immediately-after-reset
+  ;
+  ; Actually, nothing is standardized or guaranteed about
+  ; the processor state after BIOS initialization.
+  ; The only reliable value that can be leveraged are the followings.
+  ; - CS:IP points to 0x07c00, but can vary between 7c0:0000 and 000:7c00,
+  ;   or any value pair that points to 0x7c00.
+  ; - DL holds the drive number where the BIOS loaded the first sector from.
+  ; See also: https://forum.osdev.org/viewtopic.php?t=30857
 start:
 
 .bios_parameter_block:
