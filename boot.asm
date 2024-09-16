d8b1eea81ca2c6a1a9250ee104ec5b7b0bf747c5
orangecalculator <smartman1996@gmail.com>
2024-09-16 09:29:58 +0000

    Robust state dump after BIOS initialization.

--- boot.asm
+++ boot.asm
@@ -1,11 +1,20 @@
 ORG 0
 BITS 16
+  ; Actually, most of the instructions are position independent,
+  ; so the origin settings will not likely matter.
 
+  ; At this point, this binary is loaded into 0X7C00
+  ; See also: https://www.glamenv-septzen.net/en/view/6
+  ; See also: IBM Personal Computer Hardware Reference Library, Technical Reference
+  ; See also: Ralf Brown's interrupt list, INT 19 - SYSTEM - BOOTSTRAP LOADER
 start:
 
 .bios_parameter_block:
 db 0xEB, 0x3C, 0x90 ; JMP SHORT 3C NOP
 times 2 + 0x3C - ($ - $$) db 0
+
+  ; Show register values on startup
+  call printregs
 
   ; Setup segment registers for consistency
   ; Cannot use the stack until setup is complete
@@ -23,45 +32,24 @@ times 2 + 0x3C - ($ - $$) db 0
   sti
 .finish_context_setup:
 
-.start_interrupt_setup:
-  mov word[ss:0x0000], handle_div_zero
-  mov word[ss:0x0002], 0x07c0
+  pusha
 
-  int 0
+  ; Show register values after custom context setup
+  call printregs
 
-.finish_interrupt_setup:
-
-  mov cx, ss
-  mov dx, sp
+  ; Print a hello message
   mov si, hellomessage
-  call printstr
-  mov ax, cx
-  call printhex
-  mov ax, dx
-  call printhex
-  mov ax, cs
-  call printhex
-  lea ax, $
-  call printhex
+  call printstroneline
 
-  int 0
-
+  ; Dump the first sector contents in memory
   mov di, 0
-.dump_start:
-  cmp di, 0x100
-  jge .dump_finish
+  mov si, 0x100
+  call dumpmemory
 
-  mov al, ' '
-  call printchar
+  popa
 
-  mov ax, word[ss:di]
-  call printhex
-
-  add di, 2
-  jmp .dump_start
-
-.dump_finish:
-
+  ; On entry, the number of current boot disk is provided on dx
+  ; TODO: load the bootloader from disk
 
   jmp $
 
@@ -75,6 +63,94 @@ handle_div_zero:
 .div_zero_message:
   db "DIVZERO", 0
 
+printregs:
+  ; Here, we assume that the sp is setup to a legitimate address
+  ; Print order is: CS, DS, ES, SS, FS, GS, DI, SI, BP, SP, BX, DX, CX, AX
+  ; Register values will be preserved across calls,
+  ; but a certain part of the stack will be tainted.
+  ; There is actually nothing we can do about that.
+
+  pusha
+  push gs
+  push fs
+  push ss
+  push es
+  push ds
+  push cs
+
+  mov di, 0
+.start_dump_reg:
+  cmp di, 14
+  jge .finish_dump_reg
+
+  mov ax, di
+  imul ax, 2
+  mov si, ax
+  add si, sp
+  mov ax, word[ss:si]
+  call printhex
+  mov al, ' '
+  call printchar
+
+  add di, 1
+  jmp .start_dump_reg
+.finish_dump_reg:
+
+  ; To complete a full line
+  mov di, 0
+.start_print_leftovers:
+  cmp di, 10
+  jge .finish_print_leftovers
+
+  mov al, ' '
+  call printchar
+
+  add di, 1
+  jmp .start_print_leftovers
+.finish_print_leftovers:
+
+  pop ax
+  pop ax
+  pop ax
+  pop ax
+  pop ax
+  pop ax
+  popa
+  ret
+
+dumpmemory:
+
+.dump_start:
+  cmp di, si
+  jge .dump_finish
+
+  ; We assume the address of dumped region is full address,
+  ; so we use ss to use segment register with value 0.
+  mov ax, word[ss:di]
+  call printhex
+
+  mov al, ' '
+  call printchar
+
+  add di, 2
+  jmp .dump_start
+
+.dump_finish:
+  ret
+
+printstroneline:
+  push si
+  call printstr
+  pop ax
+  sub si, ax
+  mov ax, 80 + 1 ; si points next to the first null byte
+  sub ax, si
+
+  mov di, ax
+  mov al, ' '
+  call printcharntimes
+  ret
+
 printstr:
   mov bx, 0
 .call_printchar:
@@ -87,6 +163,7 @@ printstr:
   ret
 
 printhex:
+  push si
   mov bx, 0
   mov si, ax
   shr ax, 12
@@ -103,6 +180,7 @@ printhex:
   mov ax, si
   and ax, 0xf
   call printhexchar
+  pop si
   ret
 
 printhexchar:
@@ -115,6 +193,20 @@ printhexchar:
   add al, 'A' - 10
 .print:
   call printchar
+  ret
+
+printcharntimes:
+  mov si, ax
+.start_print:
+  cmp di, 0
+  je .finish_print
+
+  mov ax, si
+  call printchar
+
+  sub di, 1
+  jmp .start_print
+.finish_print:
   ret
 
 ; Arguments: character in al, cursor in bx
